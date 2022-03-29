bb45080895af7266119be05fee0b3e4bd86c9f3c
COM <COM@DESKTOP-TH36TT5>
2022-03-29 09:21:14 +0900

    restful example

--- src/main/java/net/developia/spring02/mvc1/MvcController9.java
+++ src/main/java/net/developia/spring02/mvc1/MvcController9.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,6 +17,7 @@ public class MvcController9 {
 	}
 	
 	@RequestMapping(value="/info", method=RequestMethod.POST)
+	@ResponseBody
 	public void print(@RequestBody MemoDTO dto) {
 		log.info(dto.toString());
 	}
